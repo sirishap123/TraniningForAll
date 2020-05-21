1a22aeddeadb35f66a56efaa8e6da575837da175
sirishap123 <sirishaponnekanti123@gmail.com>
2020-05-21 15:32:21 +0530

    i have added another object created

--- GitHubTranining/src/JavaTranining/FirstClass.java
+++ GitHubTranining/src/JavaTranining/FirstClass.java
@@ -10,6 +10,8 @@ public class FirstClass
 		System.out.println("i am learning GitHub");
 		
 		System.out.println("push the Commit goes to GitHub Server");
+		
+		System.out.println("commited added to central Repository");
 
 	}
 
